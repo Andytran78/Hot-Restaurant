3755dfb1d032f0bd027c87af78936c3634a7be7b
Cameron Lee Ball <cameronleeball@gmail.com>
2017-05-16 19:34:54 -0400

    adding the reservation module to app folder

---
+++ app/Reservation.js
@@ -0,0 +1,23 @@
+//Logic for creating new reservation & pushing to the appropriate array file
+
+var server = require('./server.js');
+var tables = require('./tables.js');
+var wailist = require('./waitlist.js');
+
+function Reservation(name, phone, email, id) {
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.id = id;
+};
+
+if (tables.length < 5) {
+    var newTable = new Reservation();
+    tables.push(newTable);
+    alert("Congrats! You've successfully booked a table at Hot-Restaurant!");
+} else {
+    alert("I'm sorry, there are no more tables available");
+    waitlist.push(newTable);
+}
+
+module.exports('Reservation');
