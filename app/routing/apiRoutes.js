9cf20336cdb43e8e89bb00a5b6271d7f50c34951
Cameron Lee Ball <cameronleeball@gmail.com>
2017-05-16 20:30:17 -0400

    updated routing apps

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,27 @@
 
 
 
+app.get("/api/:tables", function(req, res) {
+  v;
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  var tables = req.body;
+
+  console.log(tables);
+
+  // We then add the json the user sent to the character array
+  tables.push(newTable);
+
+  // We then display the JSON to the users
+  res.json(tables);
+});
 
 
 
 
-module.exports('apiRoutes')
+
+
+module.exports('apiRoutes');
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var server = require('./server.js');
 
 module.exports('htmlRoutes');
